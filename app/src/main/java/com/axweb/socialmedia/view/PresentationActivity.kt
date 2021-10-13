add6c144aafe4e310d8ca754e9620f04d4e5c0ef
evanvidel <evanvidel@gmail.com>
2021-10-12 22:54:57 -0300

    Update MainActivity, PresentationActivity, splashActivity

--- app/src/main/java/com/axweb/socialmedia/view/PresentationActivity.kt
+++ app/src/main/java/com/axweb/socialmedia/view/PresentationActivity.kt
@@ -1,12 +1,43 @@
 package com.axweb.socialmedia.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import com.axweb.socialmedia.R
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.axweb.socialmedia.databinding.ActivityPresentationBinding
+
 
 class PresentationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPresentationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_presentation)
+        binding = ActivityPresentationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
+
+        binding.btLogin.setOnClickListener {
+            userLogin()
+        }
+
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
+
+    private fun userLogin() {
+            val intent = Intent(this, FormLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 }
