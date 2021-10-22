e1ae5236e68c7e85a25ab5a527c711eba9eea2e6
evanvidel <evanvidel@gmail.com>
2021-10-22 10:15:43 -0300

    Add Logger in retrofit

--- app/src/main/java/com/axweb/socialmedia/service/RetrofitService.kt
+++ app/src/main/java/com/axweb/socialmedia/service/RetrofitService.kt
@@ -1,43 +1,40 @@
 package com.axweb.socialmedia.service
 
 import com.axweb.socialmedia.model.LoginResponse
-import com.axweb.socialmedia.model.User
+import com.axweb.socialmedia.model.LoginRequest
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import java.util.concurrent.TimeUnit
 
 interface RetrofitService {
 
-    @POST("/api/login")
-    fun login(@Body user: User):Call<LoginResponse>
+    @POST("/api/login/")
+    fun login(@Body request: LoginRequest):Call<LoginResponse>
 
     companion object {
 
         private val retrofitService: RetrofitService by lazy {
 
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor(logging)
+
+
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://demo-social-media-api.herokuapp.com")
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(logger)
+                .client(httpClient.build())
                 .build()
 
             retrofit.create(RetrofitService::class.java)
         }
 
-        private val logger: OkHttpClient
-            get() {
-                val httpClient = OkHttpClient.Builder()
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
 
-                return httpClient.build()
-            }
 
         fun getInstance(): RetrofitService{
             return retrofitService
